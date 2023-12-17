3ae586e005ad910edde8d4e8202c281fa531fdba
lvxiaoyi <lvxiaoyi_lv@163.com>
2023-12-17 15:33:09 +0800

    init

---
+++ src/main/java/com/example/studyguava/StudyGuavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyguava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGuavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyGuavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyguava/StudyGuavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyguava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGuavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
